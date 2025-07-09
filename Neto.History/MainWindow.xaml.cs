988c1606ab33838e52cffe9f87f8c6925fcf39a4
Mykyta <sikachyna.n@gmail.com>
2025-07-09 18:28:04 +0300

    feat: create history window UI & functionality

--- Neto.History/MainWindow.xaml.cs
+++ Neto.History/MainWindow.xaml.cs
@@ -1,27 +1,59 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using WinUIEx;
+using Neto.History.ViewModels;
 
 namespace Neto.History
 {
     public sealed partial class MainWindow : WinUIEx.WindowEx
     {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
         public MainWindow()
         {
             this.InitializeComponent();
             this.ExtendsContentIntoTitleBar = true;
+
+            ClearHistoryButton.IsEnabled = ViewModel.Records.Count > 0;
         }
+
+        private async void ClearHistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog confirmationDialog = new ContentDialog
+            {
+                Title = "Confirm",
+                Content = "Are you sure you want to clear history?",
+                PrimaryButtonText = "Yes",
+                CloseButtonText = "No",
+                XamlRoot = ClearHistoryButton.XamlRoot
+            };
+
+            ContentDialogResult result = await confirmationDialog.ShowAsync();
+            if (result == ContentDialogResult.Primary)
+            {
+                ViewModel.ClearHistory();
+            }
+
+            ClearHistoryButton.IsEnabled = ViewModel.Records.Count > 0;
+        }
+
+        private void LoadHistoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.LoadHistory();
+
+            HistoryTable.ItemsSource = ViewModel.Records;
+            ClearHistoryButton.IsEnabled = ViewModel.Records.Count > 0;
+        }
+    }
+
+    public class BoolToIconConverteer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? "\uE73E" : "\uE711";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => null;
     }
 }
---
+++ Neto.History/ViewModels/MainViewModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Neto.History.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Record> Records { get; set; }
+        
+        public MainViewModel()
+        {
+            LoadHistory();
+        }
+
+        public void LoadHistory()
+        {
+            string historyFilePath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Neto",
+                "history.json");
+
+            if (!File.Exists(historyFilePath))
+            {
+                Records = [];
+            }
+            else
+            {
+                string fileContents = File.ReadAllText(historyFilePath);
+                List<Record> list = JsonSerializer.Deserialize<List<Record>>(
+                    fileContents, 
+                    new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+                Records = new ObservableCollection<Record>(list ?? []);
+            }
+        }
+
+        public void ClearHistory()
+        {
+            Records.Clear();
+
+            string historyFilePath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Neto",
+                "history.json");
+
+            if (File.Exists(historyFilePath))
+            {
+                File.WriteAllText(historyFilePath, "[]");
+            }
+        }
+    }
+
+    public partial class Record : INotifyPropertyChanged
+    {
+        private string _date;
+        private string _time;
+        private bool _status;
+
+        [JsonPropertyName("date")]
+        public string Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged(nameof(Date));
+            }
+        }
+        [JsonPropertyName("time")]
+        public string Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+                OnPropertyChanged(nameof(Time));
+            }
+        }
+        [JsonPropertyName("status")]
+        public bool Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                OnPropertyChanged(nameof(Status));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
